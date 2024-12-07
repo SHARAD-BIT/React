51d030465a41f9fb52ff45923be1e9d922831d12
SHARAD-BIT <singhsharad095@gmail.com>
2024-12-07 17:24:42 +0530

    BASICS

---
+++ Desktop/react/components/src/functional comp/form.jsx
@@ -0,0 +1,13 @@
+ function Form() {
+    return (
+        <div>
+            <h1>form</h1>
+        <label>Username</label>
+        <input type="text"></input> 
+         <label>Password</label>
+          <input type="text"></input> 
+
+                       </div>
+    );
+ }
+ export default Form;
